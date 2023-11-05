1dbce0ea1f7c1915ac39be16c9759dda9022bdd6
RobotBruceDude <42329339+RobotBruceDude@users.noreply.github.com>
2023-11-05 08:57:07 +0000

    Create Event in Progress...

--- weatherApp/views.py
+++ weatherApp/views.py
@@ -2,7 +2,23 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
 from django.http import HttpResponse
 
+from .forms import EventForm
+
+
 def index(request):
   return render(request, 'weatherApp/index.html')
+
+
+def createEvent(request):
+  if request.method == 'POST':
+    event = EventForm(request.POST)
+  
+    if event.is_valid():
+      event.save()
+      return HttpResponse('New Event Added!')
+  else:
+    event = EventForm()
+      
+  return render(request, 'weatherApp/addEvent.html', {'createEvent': event})
 
       
